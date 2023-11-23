7a220b71fbd19fdf84a8f6e51ac64bcbe810243f
Manish <tamangmanish3456@gmail.com>
2023-11-23 14:13:46 +0545

    connect with database

---
+++ server/src/config/database.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+mongoose.set("strictQuery", false);
+
+class Database {
+   constructor(url) {
+      this.url = url;
+   }
+
+   async connect() {
+      try {
+         await mongoose.connect(this.url);
+         console.log(`
+                Connected to the database : ${mongoose.connection.db.databaseName}
+            `);
+      } catch (error) {
+         throw error;
+      }
+   }
+
+   async disconnect() {
+      try {
+         await mongoose.disconnect();
+         console.log(`
+                Disconnected from the database : ${mongoose.connection.db.databaseName}
+            `);
+      } catch (error) {
+         throw error;
+      }
+   }
+}
+
+export default Database;
--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,25 @@
 import dotenv from "dotenv";
 import app from "./app.js";
+import Database from "./config/database.js";
 
 dotenv.config({
-    path : './env'
+   path: "./env",
 });
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server up and running on port ${PORT}`));
+const db = new Database(process.env.DATABASE_URL);
+
+db.connect().catch((err) => console.error(err));
+
+process.on("SIGINT", async () => {
+   try {
+      await db.disconnect();
+      process.exit(0);
+   } catch (error) {
+      console.error(error);
+      process.exit(1);
+   }
+});
+
+app.listen(PORT, () => console.log(`⚙️ Server up and running on port ${PORT}`));
